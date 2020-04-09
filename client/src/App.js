8a8c15423ffec974ee9c6a0222328f733edb39ba
Ricardo DeRosa <rico@Ricardos-MacBook-Pro.local>
2020-04-08 21:26:12 -0400

    began adding routes to front end, do not work

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import { readAllMovies, readAllPosts } from './services/api-helper';
+import MoviesIndex from './components/Moviesindex';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    movies: [],
+    posts: []
+  }
+
+  async componentDidMount(){
+    const movies = await readAllMovies
+    const posts = await readAllPosts
+    this.setState({
+      movies,
+      posts
+    })
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      
+      <Route Path = '/movies' render={()=> {
+        <MoviesIndex
+        movies=this.state.movies
+        />
+      }}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/MoviesIndex.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MoviesIndex(props) {
+    return(
+        <div>
+
+        </div>
+    )
+}
