35433f0b3c6df00f3c7bd0f1c765efc9fcc93a62
Ricardo DeRosa <rico@Ricardos-MacBook-Pro.local>
2020-04-08 21:29:18 -0400

    compiling successfully, without routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import { readAllMovies, readAllPosts } from './services/api-helper';
-import MoviesIndex from './components/Moviesindex';
 
 class App extends Component {
   state = {
@@ -20,16 +19,12 @@ class App extends Component {
   }
 
 
-
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
       
       <Route Path = '/movies' render={()=> {
-        <MoviesIndex
-        movies=this.state.movies
-        />
       }}/>
       </div>
     );
