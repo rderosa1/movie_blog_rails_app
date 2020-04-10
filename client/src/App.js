8ca90e8834a93fdbf1b58762598685406712f60d
Ricardo DeRosa <rico@Ricardos-MacBook-Pro.local>
2020-04-09 21:44:23 -0400

    trying to add CRUD to posts, failing

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,11 @@ import PostsIndex from './components/PostsIndex';
 import Home from './components/Home'
 import Login from './components/Login'
 import Register from './components/Register'
+import { loginUser } from './services/api-helper'
+import { registerUser } from './services/api-helper'
+import { verifyUser } from './services/api-helper'
+import { removeToken } from './services/api-helper'
+import { createPost} from './services/api-helper'
 
 
 class App extends Component {
@@ -27,6 +32,24 @@ class App extends Component {
     })
   }
 
+  //================================================
+  //==================== Posts =====================
+  //================================================
+
+  //function to create a new post
+
+  addPost = async () => {
+    const newPost = await createPost(this.state.formData)
+    this.setState(prevState => ({
+      post: [...prevState.food, newPost],
+      formData: {
+        content: ""
+      }
+    }))
+  }
+
+
+
   //handleSubmit
   handleChange = (e) => {
     const { name, value } = e.target;
@@ -37,7 +60,61 @@ class App extends Component {
     this.props.history.push("/login")
   }
 
-  //handlesubmit
+
+  //================================================
+  //==================== auth ======================
+  //================================================
+
+  // Function to login a user
+  // we set the user data in state.
+  handleLogin = async () => {
+    const currentUser = await loginUser(this.state.authFormData);
+    this.setState({ currentUser })
+  }
+
+  // Function to register a user
+  // we set the user data in state.
+  handleRegister = async (e) => {
+    e.preventDefault();
+    const currentUser = await registerUser(this.state.authFormData);
+    this.setState({ currentUser })
+  }
+
+
+  handleVerify = async () => {
+    const currentUser = await verifyUser();
+    if (currentUser) {
+      this.setState({ currentUser })
+    }
+  }
+  // Function to logout user
+  // We delete the token from local storage
+  // set the current user in state back to null
+  // and call our remove token function to remove
+  // the auth headers from our api call
+  handleLogout = () => {
+    localStorage.removeItem("jwt");
+    this.setState({
+      currentUser: null
+    })
+    removeToken();
+  }
+
+
+  // Handle change function for auth forms
+  authHandleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState(prevState => ({
+      authFormData: {
+        ...prevState.authFormData,
+        [name]: value
+      }
+    }));
+  }
+
+  //================================================
+  //==================== render ====================
+  //================================================
 
 
   render() {
@@ -64,16 +141,18 @@ class App extends Component {
         <Route exact path="/login" render={(props) => (
           <Login
           formData={this.state.formData}
-          handleSubmit={this.handleLogin}
-          handleChange={this.handleChange}
+          handlelogin={this.handleLogin}
+          handleChange={this.authHandleChange}
            />)} />
+
 
         <Route exact path="/register" render={(props) => (
           <Register
-          formData={this.state.formData}
-          handleSubmit={this.handleRegister}
-          handleChange={this.handleChange}
+          formData={this.state.authFormData}
+          handleChange={this.authHandleChange}
+          handleRegister={this.handleRegister}
             />)} />
+
       </div>
     );
   }
